ed1f121809f23fbd9180c45f34da2d9c4d52f161
Khamidulla <khamidulla_orifov@epam.com>
2025-09-01 13:39:50 +0500

    add login form

--- src/app/shared/components/login-form/login-form.component.ts
+++ src/app/shared/components/login-form/login-form.component.ts
@@ -8,6 +8,5 @@ import { NgForm } from '@angular/forms';
 })
 export class LoginFormComponent {
   @ViewChild("loginForm") public loginForm!: NgForm;
-  public password: string = '';
   //Use the names `email` and `password` for form controls.
 }
